f02a53f5c6a35195b3915b3dfbcb4bca1a0a9b43
aUniqueUser <55301516+aUniqueUser@users.noreply.github.com>
2020-09-03 16:18:27 -0400

    Update exec.cpp

--- src/exec.cpp
+++ src/exec.cpp
@@ -63,9 +63,9 @@ int main(int argc, const char** argv) {
 	}
 	ReplaceString(cmd, " && ", " ; ");
 	if (cmd.length() >= 63) {
-		peer.SendMessage(0, (1 << target_id), ipc_commands::execute_client_cmd_long, cmd.c_str(), cmd.length() + 1);
+		peer.SendMessage(0, target_id, ipc_commands::execute_client_cmd_long, cmd.c_str(), cmd.length() + 1);
 	} else {
-		peer.SendMessage(cmd.c_str(), (1 << target_id), ipc_commands::execute_client_cmd, 0, 0);
+		peer.SendMessage(cmd.c_str(), target_id, ipc_commands::execute_client_cmd, 0, 0);
 	}
 
 
